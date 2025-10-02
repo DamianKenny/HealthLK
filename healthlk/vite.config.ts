2debcdc02e642985b1fdd2d8638c9394e79f973d
Damian Kenny <damiankenny007@gmail.com>
2025-10-02 16:51:24 +0530

    backend

--- healthlk/vite.config.ts
+++ healthlk/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/healthlk/", // 🧙‍♀️ 👈 This is the magic line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
